8d4d72d7dd7335dcc907a56d9a86b94580911d35
Casey O'Hare <caseyrohare@gmail.com>
2016-08-01 20:10:36 -0500

    Updated physics on press to pick up ball when trigger selected and to release ball with very simple physics

--- Assets/Scripts/PickupParent.cs
+++ Assets/Scripts/PickupParent.cs
@@ -39,5 +39,20 @@ public class PickupParent : MonoBehaviour {
 		}
 	}
 
+	void OnTriggerStay(Collider col) {
+		Debug.Log ("Collided with " + col.name + " and activated OnTriggerStay");
 
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Collided with " + col.name + " while holding down Touch");
+			// This way it is not effected by physics system. Instead it will be effected by us.
+			col.attachedRigidbody.isKinematic = true;
+			col.gameObject.transform.SetParent (this.gameObject.transform); // Our current game object is now the parent
+		}
+		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Released Touch while collding with " + col.name);
+			// Detach from us (parent) and turn on physics.
+			col.gameObject.transform.SetParent (null);
+			col.attachedRigidbody.isKinematic = false;
+		}
+	}
 }
