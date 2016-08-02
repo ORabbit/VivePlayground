f66c6201f041de4912d4364a09aaffc7d08f3cb4
Casey O'Hare <caseyrohare@gmail.com>
2016-08-01 20:00:28 -0500

    Missing files from last commit

---
+++ Assets/Scripts/PickupParent.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+	SteamVR_TrackedObject trackedObj;
+	SteamVR_Controller.Device device;
+
+	void Awake () {
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	void FixedUpdate () {
+		device = SteamVR_Controller.Input ((int)trackedObj.index);
+
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger is held 'Touch'");
+		}
+
+		if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger is held down 'Down'");
+		}
+
+		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger is released 'Up'");
+		}
+
+		if (device.GetPress (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger is pressed 'Press'");
+		}
+
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger is pressed 'Down'");
+		}
+
+		if (device.GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger is press released 'Up'");
+		}
+	}
+
+
+}
